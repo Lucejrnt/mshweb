c2ccc6b96e2b6a8828c6d5188921a952e610c343
Lucejrnt <luce.journet@2019.icam.fr>
2018-11-10 18:58:40 +0100

    Début des filtres

---
+++ pages/js/views.js
@@ -0,0 +1,63 @@
+new Vue({
+  el: '#findBikesForm',
+  data: {
+    filter1: null,
+    pw: null,
+    mail: null,
+    // MOTEUR
+    puissance : null,
+    couple_max : null,
+    rpm_cmax : null,
+    allumage : null,
+    cylindre_nb : null,
+    cylindre_cm : null,
+    architecture : null,
+    // SECURTIE
+    equipements : null,
+    frein_av : null,
+    frein_ar : null,
+    pneus : null,
+    // MANIABILITE
+    masse : null,
+    guidon : null,
+    roue_av : null,
+    roue_ar : null,
+    cadre : null,
+    // LOOK
+    type : null,
+    style : null,
+    coloris : null,
+    // CONFORT
+    suspension : null,
+    duo : null,
+    position : null,
+    hauteur : null,
+    bulle : null,
+    accessoires : null,
+    // BRICOLOAGE
+    entretiens : null,
+    transformation : null,
+
+
+    display: false,
+    options: null
+  },
+  methods: {
+    submit: function() {
+      this.display = !this.display
+    },
+    query: function() {
+      // Those values are supposed to be queried in your database
+      this.options = {
+        option1 : { id : 1, value : 'options1'},
+        option2 : { id : 2, value : 'options2'},
+        option3 : { id : 3, value : 'options3'},
+      }
+      this.puissances = {
+        puissance1 : { id : 1, value : '100cv'},
+        puissance2 : { id : 2, value : '200cv'},
+        puissance3 : { id : 3, value : '300cv'},
+      }
+    }
+  }
+});

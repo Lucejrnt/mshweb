cb4d6a111a0ce511439497ce8e2b1d1df5211d1f
Lucejrnt <luce.journet@2019.icam.fr>
2018-11-22 17:45:33 +0100

    filtre OK - conenxion to db ok

--- back.js
+++ back.js
@@ -62,68 +62,212 @@ app.get('/motos', (request, response) => {
 app.get('/power', (request, response) => {
   console.log('I received a get request. Now querying the database');
   // querying the database and putting the results in the http response
-  motos.find({}) //We get all the motos
-    .project({power : 1, _id : 0}) //We only keep the power values, and remove the ids
-    .toArray((err, res) => {
-      response.send(res);
-      //You might want to sort the values
+  motos.distinct("power", function(err, data) {
+    response.send(data.sort());
   });
 });
 
 app.get('/maxtorque', (request, response) => {
   console.log('I received a get request. Now querying the database');
   // querying the database and putting the results in the http response
-  motos.find({}) //We get all the motos
-    .project({maxtorque : 1, _id : 0}) //We only keep the power values, and remove the ids
-    .toArray((err, res) => {
-      response.send(res);
-      //You might want to sort the values
+  motos.distinct("maxtorque", function(err, data) {
+    response.send(data.sort());
   });
 });
 
 app.get('/maxtorquerpm', (request, response) => {
   console.log('I received a get request. Now querying the database');
   // querying the database and putting the results in the http response
-  motos.find({}) //We get all the motos
-    .project({maxtorquerpm : 1, _id : 0}) //We only keep the power values, and remove the ids
-    .toArray((err, res) => {
-      response.send(res);
-      //You might want to sort the values
+  motos.distinct("maxtorquerpm", function(err, data) {
+    response.send(data.sort());
   });
 });
 
 app.get('/strokenb', (request, response) => {
   console.log('I received a get request. Now querying the database');
   // querying the database and putting the results in the http response
-  motos.distinct("strokenb", {}) //We get all the motos
-    .project({strokenb : 1, _id : 0})
-    .toArray((err, res) => {
-      response.send(res);
-      //You might want to sort the values
+  motos.distinct("strokenb", function(err, data) {
+    response.send(data.sort());
   });
 });
 
 app.get('/strokecm', (request, response) => {
   console.log('I received a get request. Now querying the database');
   // querying the database and putting the results in the http response
-  motos.find({}) //We get all the motos
-    .project({strokecm : 1, _id : 0}) //We only keep the power values, and remove the ids
-    .toArray((err, res) => {
-      response.send(res);
-      //You might want to sort the values
+  motos.distinct("strokecm", function(err, data) {
+    response.send(data.sort());
   });
 });
 
 app.get('/architecture', (request, response) => {
   console.log('I received a get request. Now querying the database');
   // querying the database and putting the results in the http response
-  motos.find({}) //We get all the motos
-    .project({architecture : 1, _id : 0}) //We only keep the power values, and remove the ids
-    .toArray((err, res) => {
-      response.send(res);
-      //You might want to sort the values
-
+  motos.distinct("architecture", function(err, data) {
+    response.send(data.sort());
   });
 });
+
+app.get('/equipment', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("equipment", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/frontbrake', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("frontbrake", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/rearbrake', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("rearbrake", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/tires', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("tires", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/weight', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("weight", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/handlebar', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("handlebar", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/frame', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("frame", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/frontwheel', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("frontwheel", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/rearwheel', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("rearwheel", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/type', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("type", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/style', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("style", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/coloris', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("coloris", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/suspension', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("suspension", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/position', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("position", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/seatheight', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("seatheight", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/duo', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("duo", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/windprotection', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("windprotection", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/confortaccessories', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("confortaccessories", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/homemadeservice', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("homemadeservice", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+app.get('/transformations', (request, response) => {
+  console.log('I received a get request. Now querying the database');
+  // querying the database and putting the results in the http response
+  motos.distinct("transformations", function(err, data) {
+    response.send(data.sort());
+  });
+});
+
+
+
 
 app.listen(process.env.PORT || serverPort);
--- pages/js/views.js
+++ pages/js/views.js
@@ -1,52 +1,44 @@
 var view = new Vue({
   el: '#findBikesForm',
   data: {
-    /* choix que l'utilisateur va effectuer dans le formulaire */
+    /* Valeurs retournées par la base de données afin d'afficher les possibilités dans le formulaire */
       filter1: null,
       pw: null,
       mail: null,
       // MOTEUR
-      puissance : null,
-      power : null,
-      couple_max : null,
-      rpm_cmax : null,
-      fuel_system : null,
-      cylindre_nb : null,
-      cylindre_cm : null,
-      architecture : null,
+      powers: null, //Toutes les puissances disponibles dans la base de données
+      maxtorques: null,
+      maxtorquerpms: null,
+      strokenbs: null,
+      strokecms: null,
+      architectures: null,
       // SECURTIE
-      equipements : null,
-      frein_av : null,
-      frein_ar : null,
-      pneus : null,
+      equipments : null,
+      frontbrakes : null,
+      rearbrakes : null,
+      tiress : null,
       // MANIABILITE
-      masse : null,
-      guidon : null,
-      roue_av : null,
-      roue_ar : null,
-      cadre : null,
+      weights : null,
+      handlebars : null,
+      frontwheels : null,
+      rearwheels : null,
+      frames : null,
       // LOOK
-      type : null,
-      style : null,
-      coloris : null,
+      types : null,
+      styles : null,
+      coloriss : null,
       // CONFORT
-      suspension : null,
-      duo : null,
-      position : null,
-      hauteur : null,
-      bulle : null,
-      accessoires : null,
+      suspensions : null,
+      duos : null,
+      positions : null,
+      seatheights : null,
+      windprotections : null,
+      confortaccessoriess : null,
       // BRICOLOAGE
-      entretiens : null,
-      transformations : null,
+      homemadeservices : null,
+      transformationss : null,
 
-    /* Valeurs retournées par la base de données afin d'afficher les possibilités dans le formulaire */
-    powers: null, //Toutes les puissances disponibles dans la base de données
-    maxtorques: null,
-    maxtorquerpms: null,
-    strokenbs: null,
-    strokecms: null,
-    architectures: null,
+
 
     display: false,
     options: null
@@ -58,40 +50,109 @@ var view = new Vue({
     query: function(event) {
       // Those values are supposed to be queried in your database
       $.get( "/power", function(data) {
-          view.powers = data.map(function(el){
-            return el.power;
-          });
+          view.powers = data;
       });
 
       $.get( "/maxtorque", function(data) {
-          view.maxtorques = data.map(function(el){
-            return el.maxtorque;
-          });
+          view.maxtorques = data;
       });
 
       $.get( "/maxtorquerpm", function(data) {
-          view.maxtorquerpms = data.map(function(el){
-            return el.maxtorquerpm;
-          });
+          view.maxtorquerpms = data;
       });
 
       $.get( "/strokenb", function(data) {
-        view.strokenbs = data.map(function(el){
-            return el.strokenb;
-          });
+        view.strokenbs = data;
       });
 
       $.get( "/strokecm", function(data) {
-          view.strokecms = data.map(function(el){
-            return el.strokecm;
-          });
+          view.strokecms = data;
       });
 
       $.get( "/architecture", function(data) {
-          view.architectures = data.map(function(el){
-            return el.architecture;
-          });
+          view.architectures = data;
       });
+
+      $.get( "/equipment", function(data) {
+          view.equipments = data;
+      });
+
+      $.get( "/frontbrake", function(data) {
+          view.frontbrakes = data;
+      });
+
+      $.get( "/rearbrake", function(data) {
+          view.rearbrakes = data;
+      });
+
+      $.get( "/tires", function(data) {
+          view.tiress = data;
+      });
+
+      $.get( "/weight", function(data) {
+          view.weights = data;
+      });
+
+      $.get( "/handlebar", function(data) {
+          view.handlebars = data;
+      });
+
+      $.get( "/frame", function(data) {
+          view.frames = data;
+      });
+
+      $.get( "/frontwheel", function(data) {
+          view.frontwheels = data;
+      });
+
+      $.get( "/rearwheel", function(data) {
+          view.rearwheels = data;
+      });
+
+      $.get( "/type", function(data) {
+          view.types = data;
+      });
+
+      $.get( "/style", function(data) {
+          view.styles = data;
+      });
+
+      $.get( "/coloris", function(data) {
+          view.coloriss = data;
+      });
+
+      $.get( "/suspension", function(data) {
+          view.suspensions = data;
+      });
+
+      $.get( "/position", function(data) {
+          view.positions = data;
+      });
+
+      $.get( "/seatheight", function(data) {
+          view.seatheights = data;
+      });
+
+      $.get( "/duo", function(data) {
+          view.duos = data;
+      });
+
+      $.get( "/windprotection", function(data) {
+          view.windprotections = data;
+      });
+
+      $.get( "/confortaccessories", function(data) {
+          view.confortaccessoriess = data;
+      });
+
+      $.get( "/homemadeservice", function(data) {
+          view.homemadeservices = data;
+      });
+
+      $.get( "/transformations", function(data) {
+          view.transformationss = data;
+      });
+
     }
   }
 });

53c66a87f8d922c86dde41c3dedd40adc2dbc764
Thibault Béziers la Fosse <thiblf@gmail.com>
2018-11-22 11:08:03 +0100

    minor fix on the front end.

--- pages/js/views.js
+++ pages/js/views.js
@@ -1,70 +1,63 @@
-new Vue({
+var view = new Vue({
   el: '#findBikesForm',
   data: {
-    filter1: null,
-    pw: null,
-    mail: null,
-    // MOTEUR
-    puissance : null,
-    power : null,
-    couple_max : null,
-    rpm_cmax : null,
-    fuel_system : null,
-    cylindre_nb : null,
-    cylindre_cm : null,
-    architecture : null,
-    // SECURTIE
-    equipements : null,
-    frein_av : null,
-    frein_ar : null,
-    pneus : null,
-    // MANIABILITE
-    masse : null,
-    guidon : null,
-    roue_av : null,
-    roue_ar : null,
-    cadre : null,
-    // LOOK
-    type : null,
-    style : null,
-    coloris : null,
-    // CONFORT
-    suspension : null,
-    duo : null,
-    position : null,
-    hauteur : null,
-    bulle : null,
-    accessoires : null,
-    // BRICOLOAGE
-    entretiens : null,
-    transformations : null,
+    /* choix que l'utilisateur va effectuer dans le formulaire */
+      filter1: null,
+      pw: null,
+      mail: null,
+      // MOTEUR
+      puissance : null,
+      power : null,
+      couple_max : null,
+      rpm_cmax : null,
+      fuel_system : null,
+      cylindre_nb : null,
+      cylindre_cm : null,
+      architecture : null,
+      // SECURTIE
+      equipements : null,
+      frein_av : null,
+      frein_ar : null,
+      pneus : null,
+      // MANIABILITE
+      masse : null,
+      guidon : null,
+      roue_av : null,
+      roue_ar : null,
+      cadre : null,
+      // LOOK
+      type : null,
+      style : null,
+      coloris : null,
+      // CONFORT
+      suspension : null,
+      duo : null,
+      position : null,
+      hauteur : null,
+      bulle : null,
+      accessoires : null,
+      // BRICOLOAGE
+      entretiens : null,
+      transformations : null,
 
+    /* Valeurs retournées par la base de données afin d'afficher les possibilités dans le formulaire */
+    powers: null, //Toutes les puissances disponibles dans la base de données
 
     display: false,
-    options: null,
-
+    options: null
   },
   methods: {
     submit: function() {
       this.display = !this.display
     },
-    query: function() {
+    query: function(event) {
       // Those values are supposed to be queried in your database
-
-      $.get( "/motos", function( data ) {
-          console.log(data);
+      $.get( "/power", function(data) {
+          var powers = data.map(function(el){
+            return el.power;
+          });
+          view.powers = powers;
       });
-
-      $.get( "/power", function( data ) {
-            this.power = data;
-            console.log(data);
-        },
-
-      function ( data ) {
-           console.log("Could not get the puissances in the database");
-        }
-      );
-      console.log(this.puissance);
     }
   }
 });
